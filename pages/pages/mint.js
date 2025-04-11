6edb78121efde5d53ce52bc8045da01a44c92832
fiipe01 <dn.filipevieira@gmail.com>
2025-04-10 22:51:00 -0300

    Create mint.js

---
+++ pages/pages/mint.js
@@ -0,0 +1,8 @@
+export default function Mint() {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold mb-4">Mint NFT</h1>
+      <p className="text-lg">Simulate minting your Pepper Jack NFT on Solana Devnet here.</p>
+    </div>
+  );
+}
