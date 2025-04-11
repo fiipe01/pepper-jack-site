cffcf47997e945ad8da2a0d9baff2e7db01569d7
fiipe01 <dn.filipevieira@gmail.com>
2025-04-10 22:44:27 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold mb-4">Welcome to Pepper Jack</h1>
+      <p className="text-lg">This is the homepage for the Pepper Jack NFT Lottery Simulation on Solana Devnet.</p>
+    </div>
+  );
+}
