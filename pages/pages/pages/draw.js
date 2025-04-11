8ab1df0a2186c9120fa5607894d19394970624b3
fiipe01 <dn.filipevieira@gmail.com>
2025-04-10 22:52:50 -0300

    Create draw.js

---
+++ pages/pages/pages/draw.js
@@ -0,0 +1,8 @@
+export default function Draw() {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold mb-4">NFT Draw</h1>
+      <p className="text-lg">Here you can view the current draw and see who won the rarest Pepper Jack NFTs!</p>
+    </div>
+  );
+}
